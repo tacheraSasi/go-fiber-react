362fdce68743ce959e8f23193f21fa512e8c675a
tacheraSasi <tacherasasi@gmail.com>
2024-10-27 01:52:02 +0300

    added a simple basic pages structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,68 +1,36 @@
-import { Box, List, ThemeIcon } from "@mantine/core";
-import { CheckCircleFillIcon } from "@primer/octicons-react";
-import useSWR from "swr";
-import "./App.css";
-import AddTodo from "./components/AddTodo";
+import React from 'react'
+import { createBrowserRouter, Link, RouterProvider } from 'react-router-dom'
+import Login from './pages/Auth/Login'
+import About from './pages/About'
 
-export interface Todo {
-  id: number;
-  title: string;
-  body: string;
-  done: boolean;
-}
-
-export const ENDPOINT = "http://localhost:4000";
-
-const fetcher = (url: string) =>
-  fetch(`${ENDPOINT}/${url}`).then((r) => r.json());
-
-function App() {
-  const { data, mutate } = useSWR<Todo[]>("api/todos", fetcher);
-
-  async function markTodoAdDone(id: number) {
-    const updated = await fetch(`${ENDPOINT}/api/todosn/${id}/done`, {
-      method: "PATCH",
-    }).then((r) => r.json());
-
-    mutate(updated);
-  }
-
+const App = () => {
+  const route = createBrowserRouter([
+    {
+      path:"/",
+      element:(
+        <div>
+          <h1>Hello World</h1>
+          <Link to="about">About Us</Link>
+          <Link to="dashboard">Dashboard</Link>
+        </div>
+      ),
+      children: [
+        { path: 'about', element: <About /> },
+        { path: 'login', element: <Login /> },
+        {
+          path: 'dashboard',
+          element: (
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          ),
+        },
+      ],
+    }
+  ])
   return (
-    <Box
-      sx={(theme) => ({
-        padding: "2rem",
-        width: "100%",
-        maxWidth: "40rem",
-        margin: "0 auto",
-      })}
-    >
-      <List spacing="xs" size="sm" mb={12} center>
-        {data?.map((todo) => {
-          return (
-            <List.Item
-              onClick={() => markTodoAdDone(todo.id)}
-              key={`todo_list__${todo.id}`}
-              icon={
-                todo.done ? (
-                  <ThemeIcon color="teal" size={24} radius="xl">
-                    <CheckCircleFillIcon size={20} />
-                  </ThemeIcon>
-                ) : (
-                  <ThemeIcon color="gray" size={24} radius="xl">
-                    <CheckCircleFillIcon size={20} />
-                  </ThemeIcon>
-                )
-              }
-            >
-              {todo.title}
-            </List.Item>
-          );
-        })}
-      </List>
-
-      <AddTodo mutate={mutate} />
-    </Box>
-  );
+    <RouterProvider />
+  )
 }
 
-export default App;
+export default App
