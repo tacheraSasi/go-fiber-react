76cb69d85ae1ad6c93ee22cfb2180676bf540ac3
tacheraSasi <tacherasasi@gmail.com>
2024-11-01 00:59:23 +0300

    finished the logger

--- server/utils/errors.go
+++ server/utils/errors.go
@@ -10,7 +10,7 @@ import (
 
 // LogError sends an error notification email using EkiliRelay.
 func LogError(title, desc, location string) {
-	file, err := os.OpenFile("app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	file, err := os.OpenFile("../app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	relay := ekili.NewEkiliRelay("relay-6087f8c42d70f0650b9f023adc")
 
 	if err != nil{
@@ -18,9 +18,14 @@ func LogError(title, desc, location string) {
 	}
 
 	defer file.Close() //defer delays the execution till the surrounding function is finished
+
+   // Create a new logger that writes to the file, with date and time in each entry
+   logger := log.New(file, "LOG: ", log.LstdFlags|log.Lshortfile)
+
 	
 	// Constructing the email content with title and description
 	message := fmt.Sprintf("Error: %s\n\n Location: %s\n\n Details: %s", title,location, desc)
+	logger.Println(message)//logging the error in the ../app.log file
 	
 	// Sending the email with EkiliRelay
 	response, err := relay.SendEmail(
